4e230ef00b263f9f09072330e4935c07756b6399
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-08-25 09:50:23 -0300

    Requesting and extracting the essential data

---
+++ index.js
@@ -0,0 +1,29 @@
+import fetch from "node-fetch";
+
+async function requestData()
+{
+    let response = await fetch("https://restcountries.com/v3.1/all");
+    let countries = await response.json();
+    return countries;
+}
+
+async function essentialData()
+{
+    let data = await requestData();
+
+    return data.map(country => ({
+        name: country.name.common,
+        capital: country.capital,
+        area: country.area,
+        curreny: country.currencies
+    }))
+}
+
+async function showData()
+{
+    let data = await essentialData();
+
+    console.log(data);
+}
+
+showData();
